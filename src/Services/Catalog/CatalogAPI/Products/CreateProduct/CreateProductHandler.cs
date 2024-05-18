d3b0b65281a6537a65218bdbb977418a929a7b82
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2024-05-18 22:56:06 +0200

    Add creating the product

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, 
+    string ImageUrl, decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+ 
+        // Business logic to create a product
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageUrl = command.ImageUrl,
+            Price = command.Price
+        };
+
+        //saave the product to the database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
+
