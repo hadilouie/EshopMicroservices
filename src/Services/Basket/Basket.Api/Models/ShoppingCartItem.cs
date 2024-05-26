9d6cf7238c371ab42b35c6d97e9832a15dd710a5
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2024-05-26 11:18:51 +0200

    Add Basket Models

---
+++ src/Services/Basket/Basket.Api/Models/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.Api.Models;
+
+public class ShoppingCartItem
+{
+    public int quantity { get; set; } =default!;
+    public string color { get; set; } = default!;
+    public decimal price { get; set; } = default!;
+    public Guid productId { get; set; } = default!;
+    public string productName { get; set; } = default!;
+}
