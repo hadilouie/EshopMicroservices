0980dbbf01c26fd15017825b54688c69b57cdc1a
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2024-06-24 19:18:28 +0200

    Discount Grpc implementation!

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,11 +1,19 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
 var app = builder.Build();
+
+app.UseMigration();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
