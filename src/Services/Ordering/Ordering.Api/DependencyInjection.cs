252b65f10be04bc7e9466bd1b6560906e6896757
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2024-12-28 00:28:53 +0100

    Add DDD and clean architecture structure for ordering services and alll project dependencies and DIs

---
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // Add services to the container.
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline
+
+        return app;
+    }
+}
