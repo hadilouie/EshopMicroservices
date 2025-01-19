2529fcf482843372fd261be62419afba9c4b1f7d
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2025-01-19 16:11:40 +0100

    Configrue API and all dependencies

--- src/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,10 +1,19 @@
-﻿namespace Ordering.Api;
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Add services to the container.
+
+        services.AddCarter();
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -12,6 +21,14 @@ public static class DependencyInjection
     public static WebApplication UseApiServices(this WebApplication app)
     {
         // Configure the HTTP request pipeline
+        app.UseExceptionHandler(options => { });
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+        app.MapCarter();
 
         return app;
     }
