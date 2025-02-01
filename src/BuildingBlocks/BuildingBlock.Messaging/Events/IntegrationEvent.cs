eb34df83eef476e9c218bb1dffb11cafd974bc94
Mohammadhadi Aghalouyaghmiyouni <mohammadhadi.aghalouyaghmiyouni@nomadrail.com>
2025-02-02 00:28:18 +0100

    Add buildingBlock.Messaging

---
+++ src/BuildingBlocks/BuildingBlock.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BuildingBlock.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
